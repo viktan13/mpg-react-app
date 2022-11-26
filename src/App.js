a2acb4bacdaf3e9c81c4f236d3d23fcb60469571
Viktor <vik.tan@hotmail.com>
2022-11-25 18:23:26 -0600

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [tank, setTank] = useState(0);
+    const [mpg, setMpg] = useState(0);
+    const [distance, setDistance] = useState(0);
+    const [result, setResult] = useState('');
+
+
+    function onCalculate() {
+        const carDistance = tank * mpg;
+        if(carDistance >= distance) setResult('We will reach the gas station.');
+        else setResult('We will not get to the gas station')
+    }
+
+    return (
+        <div className="App">
+
+            <div>
+                <label htmlFor="tank">Tank Size</label>
+                <input value={tank} onChange={e => setTank(e.target.value)} name="tank" type="number"/>
+            </div>
+
+            <div>
+                <label htmlFor="mpg">MPG</label>
+                <input value={mpg} onChange={e => setMpg(e.target.value)} name="mpg" type="number"/>
+            </div>
+
+            <div>
+                <label htmlFor="distance">Distance</label>
+                <input value={distance} onChange={e => setDistance(e.target.value)} name="distance" type="number"/>
+            </div>
+
+            <button onClick={onCalculate}>Calculate</button>
+
+            <hr/>
+
+            {result}
+
+        </div>
+    );
 }
 
 export default App;
